0af52a993cfde401c97f7a44f364fb6a42e814af
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-16 22:25:10 -0500

    commit tree 562fcdc69bd03db00448e38709b9c9ce8b8f6e19

--- index.js
+++ index.js
@@ -1,1 +1,127 @@
 // Write your solution here!
+
+const cats=["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return console.log(cats);
+}
+destructivelyAppendCat('Ralph');
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield')
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return console.log(cats);
+}
+
+destructivelyPrependCat('Bob');
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return console.log(cats);
+
+}
+destructivelyRemoveLastCat();
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return console.log(cats);
+}
+
+destructivelyRemoveFirstCat();
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+function appendCat(name) {
+    const newCats=cats.slice();
+    newCats.push(name);
+    return newCats;
+
+}
+console.log(appendCat('Broom'))
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+function prependCat(name) {
+    const pCats=cats.slice();
+        pCats.unshift(name)
+        return pCats
+}
+
+console.log(prependCat('Arnold'));
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+function removeLastCat() {
+    const zCats=cats.slice();
+    zCats.pop();
+    return zCats;
+}
+
+console.log(removeLastCat())
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+function removeFirstCat() {
+    const aCats=cats.slice();
+    aCats.shift();
+    return aCats;
+}
+
+console.log(removeFirstCat());
+
+cats.length = 0;
+
+cats.push('Milo', 'Otis', 'Garfield');
+
+
+
+
+
+
+
+
+
